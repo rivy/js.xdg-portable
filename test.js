5cb86161ab7f95e9f38f35612168199e537cb988
Roy Ivy III <rivy.dev@gmail.com>
2019-06-30 13:15:17 -0500

    refactor ~ improve tests

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
+'use strict';
+
 import path from 'path';
+
 import {serial as test} from 'ava';
 import importFresh from 'import-fresh';
 
@@ -33,17 +36,19 @@ test('.state', t => {
 });
 
 test('.configDirs', t => {
+	process.env.XDG_CONFIG_HOME = 'config_home';
 	process.env.XDG_CONFIG_DIRS = ['dirs', 'config_dirs'].join(path.delimiter);
 	const xdg = importFresh('.');
-	t.is(xdg.configDirs[0], 'config');
+	t.is(xdg.configDirs[0], 'config_home');
 	t.is(xdg.configDirs[1], 'dirs');
 	t.is(xdg.configDirs[2], 'config_dirs');
 });
 
 test('.dataDirs', t => {
+	process.env.XDG_DATA_HOME = 'data_home';
 	process.env.XDG_DATA_DIRS = ['dirs', 'data_dirs'].join(path.delimiter);
 	const xdg = importFresh('.');
-	t.is(xdg.dataDirs[0], 'data');
+	t.is(xdg.dataDirs[0], 'data_home');
 	t.is(xdg.dataDirs[1], 'dirs');
 	t.is(xdg.dataDirs[2], 'data_dirs');
 });
