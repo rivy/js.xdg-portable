c9873b5934f363bd7b6af139cb44ffb261110553
Roy Ivy III <rivy.dev@gmail.com>
2021-01-24 16:48:24 -0600

    docs ~ fix ESLint complaints in examples

--- eg/show-xdg-paths-detailed.js
+++ eg/show-xdg-paths-detailed.js
@@ -1,6 +1,9 @@
+/* eslint-env es6, node */
 'use strict';
 
 const xdg = require('../src/lib');
+
+/* eslint-disable no-console , security/detect-object-injection, security-node/detect-crlf */
 
 console.log('xdg.name:', xdg.name);
 console.log({ xdg });
@@ -8,3 +11,5 @@ console.log({ xdg });
 Object.keys(xdg).forEach((key) => {
 	console.log(key, '=', xdg[key]());
 });
+
+/* eslint-enable no-console , security/detect-object-injection, security-node/detect-crlf */
--- eg/show-xdg-paths.js
+++ eg/show-xdg-paths.js
@@ -1,7 +1,12 @@
+/* eslint-env es6, node */
 'use strict';
 
 const xdg = require('../src/lib');
 
+/* eslint-disable no-console , security/detect-object-injection, security-node/detect-crlf */
+
 Object.keys(xdg).forEach((key) => {
 	console.log(key, '=', xdg[key]());
 });
+
+/* eslint-enable no-console , security/detect-object-injection, security-node/detect-crlf */
