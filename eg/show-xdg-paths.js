1323e167c8763b50e277a9742e1c563e468a909e
Roy Ivy III <rivy.dev@gmail.com>
2021-01-24 17:31:16 -0600

    docs ~ wip! - initial example updates for TypeScript 'dist' project model

--- eg/show-xdg-paths.js
+++ eg/show-xdg-paths.js
@@ -1,12 +1,12 @@
 /* eslint-env es6, node */
 'use strict';
 
-const xdg = require('../src/lib');
+const xdg = require('../dist/cjs/index.js');
 
 /* eslint-disable no-console , security/detect-object-injection, security-node/detect-crlf */
 
 Object.keys(xdg).forEach((key) => {
-	console.log(key, '=', xdg[key]());
+	console.log(key, '=', typeof xdg[key] === 'function' ? xdg[key]() : xdg[key]);
 });
 
 /* eslint-enable no-console , security/detect-object-injection, security-node/detect-crlf */
