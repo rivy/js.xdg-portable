bd45f3d16f30a2ec95dd380ec95f52677122607c
Roy Ivy III <rivy.dev@gmail.com>
2021-02-12 23:06:45 -0600

    tests ~ add further dist/exports testing

--- test/dist.test.js
+++ test/dist.test.js
@@ -21,14 +21,29 @@ const packageESMPath = path.resolve(__dirname, packagePath, '..', pkg.exports['.
 const vNodeJS = process.versions.node.split('.');
 const vNodeJSMajor = +vNodeJS[0];
 
+function isObject(obj) {
+	return Object.prototype.toString.call(obj) === '[object Object]';
+}
+
+function flattenToValues(obj) {
+	const values = [];
+	if (isObject(obj) || Array.isArray(obj)) {
+		Object.keys(obj).forEach((key) => {
+			// eslint-disable-next-line security/detect-object-injection
+			values.push(...flattenToValues(obj[key]));
+		});
+	} else values.push(obj);
+	return values;
+}
+
 if (!process.env.NPM_CONFIG_TEST_DIST) {
 	test('skipped (enable with `--test-dist`)', (t) => t.pass());
 } else {
-	const tName = 'CJS <=> ESM';
+	const testID$CJStoESM = 'CJS/ESM equivalence';
 	if (vNodeJSMajor < 12) {
-		test.skip(tName + ' (requires Node-v12+)', () => void 0);
+		test.skip(testID$CJStoESM + ' (requires Node-v12+)', () => void 0);
 	} else {
-		test(tName, async (t) => {
+		test(testID$CJStoESM, async (t) => {
 			// eslint-disable-next-line security/detect-non-literal-require , security-node/detect-non-literal-require-calls
 			const mCJS = require(packageCJSPath);
 			const mESM = (await import('file:/' + packageESMPath)).default;
@@ -72,16 +87,55 @@ if (!process.env.NPM_CONFIG_TEST_DIST) {
 			t.true(fs.existsSync(pkg.exports['./cjs'].require));
 		}
 
+		// 'types' default to the Deno/ESM/TypeScript variant
 		if (pkg.exports['./esm']) {
 			const pathImport = pkg.exports['./esm'].import;
 			t.is(pkg.exports['.'].import, pathImport);
 			t.is(pkg.exports['.'].types, path.pkg.exports['./esm'].types);
-			// const extension = path.extname(pathImport);
-			// const basename = path.basename(pathImport, extension);
-			// const dirname = path.dirname(pathImport);
-			// t.is(pkg.exports['./esm'].types, path.posix.join(dirname, basename) + '.d.ts');
 			t.true(fs.existsSync(pkg.exports['./esm'].require));
 		}
 		/* eslint-enable security/detect-non-literal-fs-filename */
+	});
+
+	test("package 'exports' all exist", (t) => {
+		const exports_ = pkg.exports;
+		const paths = flattenToValues(exports_);
+		t.log({ exportsPaths: paths });
+		paths.forEach((p) => {
+			const path_ = path.resolve(__dirname, packagePath, '..', p);
+			// eslint-disable-next-line security/detect-non-literal-fs-filename
+			const exists = fs.existsSync(path_);
+			if (!exists) {
+				t.log({ path_, exists });
+			}
+			t.true(exists);
+		});
+	});
+
+	test("package 'exports' sub-paths support older tools", (t) => {
+		// confirm package files/directories exist which correspond to advertised exports sub-paths
+		// [why]: older tools import/require based on package directory structure, not 'exports'
+		const exports_ = pkg.exports;
+		const subPaths = Object.keys(exports_);
+		t.log({ subPaths });
+		// test for sub-path file/directory existence
+		subPaths.forEach((p) => {
+			const path_ = path.resolve(__dirname, packagePath, '..', p);
+			// eslint-disable-next-line security/detect-non-literal-fs-filename
+			const exists = fs.existsSync(path_);
+			if (!exists) {
+				t.log({ exists, path_ });
+			}
+			t.true(exists);
+		});
+		const files = pkg.files;
+		// test that sub-path file/directory is included in 'files'
+		subPaths.forEach((p) => {
+			const included = p === '.' || files.includes(p.replace(/^.\//, ''));
+			if (!included) {
+				t.log({ included, p, files });
+			}
+			t.true(included);
+		});
 	});
 }
