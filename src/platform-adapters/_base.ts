94f4687db2d883ce88167e73fe674c05ae914eb1
Roy Ivy III <rivy.dev@gmail.com>
2021-02-14 00:45:03 -0600

    docs ~ add JSDocs tags to Platform.Adapter methods and properties

--- src/platform-adapters/_base.ts
+++ src/platform-adapters/_base.ts
@@ -1,14 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Platform {
 	export type Adapter = {
-		readonly env: { readonly get: (_: string) => string | undefined };
-		readonly osPaths: { readonly home: () => string | undefined; readonly temp: () => string };
+		readonly env: {
+			/** @function Returns the value of the named environment variable. */
+			readonly get: (name: string) => string | undefined;
+		};
+		readonly osPaths: {
+			/** @function Returns the path string of the user's home directory (or `undefined` if the user's home directory is not resolvable). */
+			readonly home: () => string | undefined;
+			/** @function Returns the path string of the system's default directory for temporary files. */
+			readonly temp: () => string;
+		};
 		readonly path: {
+			/** Path list delimiter */
 			readonly delimiter: string;
-			readonly join: (..._: readonly string[]) => string;
-			readonly normalize: (_: string) => string;
+			/** @function Returns all path segments, joined using the platform-specific separator, and normalized. */
+			readonly join: (...paths: readonly string[]) => string;
+			/** @function Returns the normalized path, resolving all `.` and `..` segments. */
+			readonly normalize: (path: string) => string;
 		};
 		readonly process: {
+			/** OS/platform identity string */
 			readonly platform: string;
 		};
 	};
