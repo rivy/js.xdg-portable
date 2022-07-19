07f7c83d91eb60026c731a143b09a11769d39535
Roy Ivy III <rivy.dev@gmail.com>
2022-07-19 14:43:26 -0500

    maint/dev ~ update Prettier configuration/ignores

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,12 +5,13 @@
 // spell-checker:ignore (people) Roy Ivy III * rivy
 
 module.exports = {
+	// $schema: 'http://json.schemastore.org/prettierrc',
 	printWidth: 100,
 	proseWrap: 'preserve',
 	singleQuote: true,
 	useTabs: true,
 	tabWidth: 2,
-	// "prettier # overrides/[*.markdown]/tabWidth": "// set to 4 when https://github.com/prettier/prettier/issues/5019 is fixed",
+	// "# overrides/[*.markdown]/tabWidth": "// set to 4 when https://github.com/prettier/prettier/issues/5019 is fixed",
 	overrides: [
 		{
 			files: ['*.md', '*.mkd', '*.markdown'],
