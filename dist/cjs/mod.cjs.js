ccd42c1452d853d2f76e35e25302322276d960eb
Roy Ivy III <rivy.dev@gmail.com>
2025-03-22 23:32:02 -0500

    maint/dist ~ update

--- dist/cjs/mod.cjs.js
+++ dist/cjs/mod.cjs.js
@@ -2,4 +2,4 @@
 var XDG_js_1 = require("./lib/XDG.js");
 var node_js_1 = require("./platform-adapters/node.js");
 module.exports = XDG_js_1.Adapt(node_js_1.adapter).XDG;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kLmNqcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9tb2QuY2pzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSx1Q0FBcUM7QUFFckMsdURBQXNEO0FBRXRELGlCQUFTLGNBQUssQ0FBQyxpQkFBTyxDQUFDLENBQUMsR0FBVSxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kLmNqcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9tb2QuY2pzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFDQSx1Q0FBcUM7QUFDckMsdURBQXNEO0FBRXRELGlCQUFTLGNBQUssQ0FBQyxpQkFBTyxDQUFDLENBQUMsR0FBVSxDQUFDIn0=
